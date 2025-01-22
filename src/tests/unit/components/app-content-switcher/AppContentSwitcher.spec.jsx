29fb9595d380d4621ceae59ee6dd9b12e109df06
Volodymyr Biletskyi <51093342+bilets@users.noreply.github.com>
2025-01-22 15:15:12 +0200

    Apply suggestions from code review
    Co-authored-by: vlad-khrychov <67462207+vlad-khrychov@users.noreply.github.com>

--- src/tests/unit/components/app-content-switcher/AppContentSwitcher.spec.jsx
+++ src/tests/unit/components/app-content-switcher/AppContentSwitcher.spec.jsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { describe, it, vi, expect } from 'vitest'
 import AppContentSwitcher from '~/components/app-content-switcher/AppContentSwitcher'
 
 const mockOnChange = vi.fn()
