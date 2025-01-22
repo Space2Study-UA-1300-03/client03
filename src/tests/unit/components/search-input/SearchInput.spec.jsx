d68aac598d8700029c96150a02816ee0a66ec722
ANNA124kr <ann.krav4uk13@gmail.com>
2025-01-22 16:49:07 +0200

    create SearchInput.spec.js

---
+++ src/tests/unit/components/search-input/SearchInput.spec.jsx
@@ -0,0 +1,52 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { it, expect } from 'vitest'
+import SearchInput from '~/components/search-input/SearchInput'
+
+const mockedSearchText = 'test text'
+const mockedSearchEmptyString = undefined || ''
+const mockFn = vi.fn()
+
+describe('SearchInput', () => {
+  it('should render text correctly', () => {
+    const handleChange = vi.fn()
+    render(<SearchInput onChange={handleChange} search={mockedSearchText} />)
+    const input = screen.getByRole('textbox')
+    fireEvent.change(input, { target: { value: mockedSearchText } })
+    expect(input.value).toBe(mockedSearchText)
+  })
+
+  it('should call setSearch when search icon is clicked', async () => {
+    render(<SearchInput setSearch={mockFn} />)
+    const searchIcon = screen.getByTestId('search-icon')
+    await userEvent.click(searchIcon)
+    expect(mockFn).toHaveBeenCalled()
+    // expect(mockFn).toBeCalled()
+  })
+
+  it('should call setSearch with empty string when delete icon is clicked', async () => {
+    render(<SearchInput setSearch={mockFn} />)
+    const deleteIcon = screen.getByTestId('delete-icon')
+    await userEvent.click(deleteIcon)
+    expect(mockFn).toHaveBeenCalledWith('' || undefined)
+  })
+
+  it('should call setSearch when enter is pressed', () => {
+    render(<SearchInput setSearch={mockFn} />)
+    const searchIcon = screen.getByTestId('search-icon')
+    fireEvent.keyPress(searchIcon)
+    expect(mockFn).toHaveBeenCalled()
+  })
+
+  it('should have hidden class if search is empty', () => {
+    render(<SearchInput search={mockedSearchEmptyString} />)
+    const deleteIcon = screen.getByTestId('delete-icon')
+    expect(deleteIcon).toHaveClass('hidden')
+  })
+
+  it('should have visible class if search is not empty', () => {
+    render(<SearchInput search={mockedSearchText} />)
+    const deleteIcon = screen.getByTestId('delete-icon')
+    expect(deleteIcon).toHaveClass('visible')
+  })
+})
