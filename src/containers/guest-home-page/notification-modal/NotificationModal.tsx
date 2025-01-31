8c143875803c1df53633f047c87b928de5e175e3
Anna Kravchuk <132332524+ANNA124kr@users.noreply.github.com>
2025-01-31 11:45:16 +0200

    Feature/#66 create informational pop up window (#36)
    create information pop up window

--- src/containers/guest-home-page/notification-modal/NotificationModal.tsx
+++ src/containers/guest-home-page/notification-modal/NotificationModal.tsx
@@ -4,31 +4,40 @@ import { Box } from '@mui/material'
 import ImgTitleDescription from '~/components/img-title-description/ImgTitleDescription'
 import AppButton from '~/components/app-button/AppButton'
 import { styles } from '~/containers/guest-home-page/notification-modal/NotificationModal.styles'
+import imgInfo from '~/assets/img/guest-home-page/info.svg'
+import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
 
 interface ConfirmEmailModal {
   description: string | ReactElement
   buttonTitle: string
   title: string
   img: string
+  userEmail?: string
   onClose: () => void
 }
 
 const NotificationModal: FC<ConfirmEmailModal> = ({
-  description,
-  buttonTitle,
-  title,
-  img,
-  onClose
+  userEmail = 'Your email'
 }) => {
+  const { t } = useTranslation()
+  const nav = useNavigate()
+
   return (
     <Box sx={styles.root}>
       <ImgTitleDescription
-        description={description}
-        img={img}
+        description={`${t('signup.confirmEmailMessage')}${userEmail}${t('signup.confirmEmailDesc')}`}
+        img={imgInfo}
         style={styles.imgTitleDesc}
-        title={title}
+        title={t('signup.confirmEmailTitle')}
       />
-      <AppButton onClick={onClose}>{buttonTitle}</AppButton>
+      <AppButton
+        onClick={() => {
+          nav('/')
+        }}
+      >
+        {t('common.confirmButton')}
+      </AppButton>
     </Box>
   )
 }
