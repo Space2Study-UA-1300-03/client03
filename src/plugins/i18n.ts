d4df749d9eaeba89c678d06c93a29223211b8289
Oleksandr Ostapiuk <ostapiuksanekk@gmail.com>
2025-01-31 10:02:45 +0200

    Added language switcher

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -2,10 +2,22 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import resources from '~/constants/translations'
 
+export const supportedLngs = {
+  en: 'English',
+  ua: 'Українська'
+}
+
+const storedLanguage = localStorage.getItem('i18nextLng')
+
 void i18n.use(initReactI18next).init({
   resources,
-  lng: 'en',
-  ns: ['translations']
+  lng: storedLanguage || 'en',
+  ns: ['translations'],
+  supportedLngs: Object.keys(supportedLngs),
+  interpolation: {
+    escapeValue: false
+  },
+  fallbackLng: 'en'
 })
 
 i18n.languages = ['en', 'ua']
