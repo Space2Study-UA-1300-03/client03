21f815d9f2aae76ce991ac456f43ba0809a8aabc
SanekOstapiuk <70436744+SanekOstapiuk@users.noreply.github.com>
2025-01-31 16:13:59 +0200

    hotfix email modal (#40)

--- src/containers/guest-home-page/notification-modal/NotificationModal.tsx
+++ src/containers/guest-home-page/notification-modal/NotificationModal.tsx
@@ -4,40 +4,31 @@ import { Box } from '@mui/material'
 import ImgTitleDescription from '~/components/img-title-description/ImgTitleDescription'
 import AppButton from '~/components/app-button/AppButton'
 import { styles } from '~/containers/guest-home-page/notification-modal/NotificationModal.styles'
-import imgInfo from '~/assets/img/guest-home-page/info.svg'
-import { useTranslation } from 'react-i18next'
-import { useNavigate } from 'react-router-dom'
 
 interface ConfirmEmailModal {
   description: string | ReactElement
   buttonTitle: string
   title: string
   img: string
-  userEmail?: string
   onClose: () => void
 }
 
 const NotificationModal: FC<ConfirmEmailModal> = ({
-  userEmail = 'Your email'
+  description,
+  buttonTitle,
+  title,
+  img,
+  onClose
 }) => {
-  const { t } = useTranslation()
-  const nav = useNavigate()
-
   return (
     <Box sx={styles.root}>
       <ImgTitleDescription
-        description={`${t('signup.confirmEmailMessage')}${userEmail}${t('signup.confirmEmailDesc')}`}
-        img={imgInfo}
+        description={description}
+        img={img}
         style={styles.imgTitleDesc}
-        title={t('signup.confirmEmailTitle')}
+        title={title}
       />
-      <AppButton
-        onClick={() => {
-          nav('/')
-        }}
-      >
-        {t('common.confirmButton')}
-      </AppButton>
+      <AppButton onClick={onClose}>{buttonTitle}</AppButton>
     </Box>
   )
 }
