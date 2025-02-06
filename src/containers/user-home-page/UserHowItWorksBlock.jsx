c6628be91efb94496e99da6862cbe8b0162e2c66
Iryna Khylchuk <khylchuk@icloud.com>
2025-02-06 12:59:47 +0200

    Implemented "HowItWorks" step block
    Created the "UserHowItWorksBlock" component.
    In the future, this component will be used in the "UserHowItWorks" container, which will dynamically change its text content based on the user role.

---
+++ src/containers/user-home-page/UserHowItWorksBlock.jsx
@@ -0,0 +1,21 @@
+import { useTranslation } from 'react-i18next'
+import { Box } from '@mui/material'
+
+import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
+
+const UserHowItWorksBlock = ({ image, title, description, style, key }) => {
+  const { t } = useTranslation()
+
+  return (
+    <Box key={key} style={style.card}>
+      <Box alt={title} component='img' src={image} sx={style.cardImg} />
+      <TitleWithDescription
+        description={t(description)}
+        style={style.cardTitleWithDescription}
+        title={t(title)}
+      />
+    </Box>
+  )
+}
+
+export default UserHowItWorksBlock
