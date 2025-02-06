6516f3b9d9437951211b7d9f7648c5a717e792df
Iryna Khylchuk <khylchuk@icloud.com>
2025-02-06 13:18:03 +0200

    Removed key

--- src/containers/user-home-page/UserHowItWorksBlock.jsx
+++ src/containers/user-home-page/UserHowItWorksBlock.jsx
@@ -3,11 +3,11 @@ import { Box } from '@mui/material'
 
 import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
 
-const UserHowItWorksBlock = ({ image, title, description, style, key }) => {
+const UserHowItWorksBlock = ({ image, title, description, style }) => {
   const { t } = useTranslation()
 
   return (
-    <Box key={key} style={style.card}>
+    <Box style={style.card}>
       <Box alt={t(title)} component='img' src={image} sx={style.cardImg} />
       <TitleWithDescription
         description={t(description)}
