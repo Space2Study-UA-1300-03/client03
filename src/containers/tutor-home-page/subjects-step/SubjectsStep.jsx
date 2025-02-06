a1aca356ce2be9b05987d8b8d7ad664ff74a087e
Volodymyr Biletskyi <51093342+bilets@users.noreply.github.com>
2025-02-06 14:04:21 +0200

    Feature/#63 add button and chips to subjects step (#53)
    * added button and chips to Subjects step

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
@@ -1,21 +1,19 @@
 import { useState, useMemo } from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-
+import AppChipList from '~/components/app-chips-list/AppChipList'
 import AppAutoComplete from '~/components/app-auto-complete/AppAutoComplete'
 import { categoriesMock } from '~/containers/tutor-home-page/subjects-step/constants'
 import { useTranslation } from 'react-i18next'
-
 import { styles } from '~/containers/tutor-home-page/subjects-step/SubjectsStep.styles'
 import img from '~/assets/img/tutor-home-page/become-tutor/study-category.svg'
 import AppButton from '~/components/app-button/AppButton'
 
 const SubjectsStep = ({ btnsBox }) => {
   const { t } = useTranslation()
-  const [subjects, setSubjects] = useState({
-    category: null,
-    subject: null
-  })
+  const [subjects, setSubjects] = useState({ category: null, subject: null })
+  const [listOfItems, setListOfItems] = useState([])
+  const sameSubjectError = listOfItems.includes(subjects.subject)
 
   const categoryOptions = useMemo(
     () => categoriesMock.map((item) => Object.keys(item)[0]),
@@ -23,19 +21,30 @@ const SubjectsStep = ({ btnsBox }) => {
   )
 
   const subjectOptions = useMemo(() => {
-    return subjects.category
-      ? categoriesMock.find(
-          (cat) => Object.keys(cat)[0] === subjects.category
-        )?.[subjects.category] || []
-      : []
+    if (!subjects.category) return []
+    return (
+      categoriesMock.find((cat) => Object.keys(cat)[0] === subjects.category)?.[
+        subjects.category
+      ] || []
+    )
   }, [subjects.category])
 
-  const onChangeCategory = (_, value) => {
+  const onChangeCategory = (_, value) =>
     setSubjects({ category: value, subject: null })
+
+  const onChangeSubject = (_, value) =>
+    setSubjects((prev) => ({ ...prev, subject: value }))
+
+  const addSubject = () => {
+    if (sameSubjectError) {
+      return
+    }
+    setListOfItems((prev) => [...prev, subjects.subject])
+    setSubjects({ category: null, subject: null })
   }
 
-  const onChangeSubject = (_, value) => {
-    setSubjects((prev) => ({ category: prev.category, subject: value }))
+  const handleChipDelete = (item) => {
+    setListOfItems((prev) => prev.filter((name) => name !== item))
   }
 
   return (
@@ -45,11 +54,11 @@ const SubjectsStep = ({ btnsBox }) => {
       </Box>
       <Box sx={styles.rightBox}>
         <Box sx={styles.contentBox}>
-          <Typography mb='20px'>{t('becomeTutor.categories.title')}</Typography>
+          <Typography mb={2}>{t('becomeTutor.categories.title')}</Typography>
           <AppAutoComplete
             onChange={onChangeCategory}
             options={categoryOptions}
-            sx={{ mb: '20px' }}
+            sx={{ mb: 2 }}
             textFieldProps={{
               label: t('becomeTutor.categories.mainSubjectsLabel')
             }}
@@ -59,15 +68,28 @@ const SubjectsStep = ({ btnsBox }) => {
             disabled={!subjects.category}
             onChange={onChangeSubject}
             options={subjectOptions.map((item) => item.name)}
-            sx={{ mb: '16px' }}
-            textFieldProps={{
-              label: t('becomeTutor.categories.subjectLabel')
-            }}
+            sx={{ mb: 2 }}
+            textFieldProps={{ label: t('becomeTutor.categories.subjectLabel') }}
             value={subjects.subject}
           />
-          <AppButton disabled={!subjects.subject} fullWidth>
+          <AppButton
+            disabled={!subjects.subject || sameSubjectError}
+            fullWidth
+            onClick={addSubject}
+            sx={{ mb: 2 }}
+          >
             {t('becomeTutor.categories.btnText')}
           </AppButton>
+          {sameSubjectError && (
+            <Typography sx={{ textAlign: 'center', color: 'red', mb: 2 }}>
+              {t('becomeTutor.categories.sameSubject')}
+            </Typography>
+          )}
+          <AppChipList
+            defaultQuantity={2}
+            handleChipDelete={handleChipDelete}
+            items={listOfItems}
+          />
         </Box>
         {btnsBox}
       </Box>
