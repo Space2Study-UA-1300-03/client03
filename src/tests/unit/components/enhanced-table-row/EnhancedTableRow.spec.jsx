e069ace3dc4e39b222f39e1afc73fbaf755bcad9
ANNA124kr <ann.krav4uk13@gmail.com>
2025-02-09 21:13:21 +0200

    create test enhanced table row

--- src/tests/unit/components/enhanced-table-row/EnhancedTableRow.spec.jsx
+++ src/tests/unit/components/enhanced-table-row/EnhancedTableRow.spec.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { describe, expect, it } from 'vitest'
 import EnhancedTableRow from '~/components/enhanced-table/enhanced-table-row/EnhancedTableRow'
+import { MenuItem, Checkbox, Menu } from '@mui/material'
 
 const mockedUseNavigate = vi.fn()
 vi.mock('react-router-dom', async () => {
@@ -16,53 +16,90 @@ vi.mock('react-router-dom', async () => {
 })
 
 const columsMock = []
-const isSelectionMock = ''
-const itemMock = {}
-const refetchDataMock = ''
 const rowActionsMock = []
-const onRowClickMock = vi.fn()
-const selectMock = {}
+const isSelectionMock = ''
+const refetchDataMock = ''
 const selectedRowsMock = ''
-const keyMock = 1
+const keyMock = '1'
+const itemMock = {}
+const selectMock = {}
+const onRowClickMock = vi.fn()
 const handleSelectClick = vi.fn()
+const onActionMock = vi.fn()
+
+const item1Mock = 'item 1'
+const item2Mock = 'item 2'
+
+const renderComponent = (props = {}) =>
+  render(
+    <EnhancedTableRow
+      columns={columsMock}
+      isSelection={isSelectionMock}
+      item={itemMock}
+      key={keyMock}
+      onChange={handleSelectClick}
+      onRowClick={onRowClickMock}
+      refetchData={refetchDataMock}
+      rowActions={rowActionsMock}
+      select={selectMock}
+      selectedRows={selectedRowsMock}
+      {...props}
+    />
+  )
 
 describe('EnhancedTableRow', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+    renderComponent()
+  })
+
   it('should render table row with correct data', () => {
-    render(
-      <EnhancedTableRow
-        columns={columsMock}
-        isSelection={isSelectionMock}
-        item={itemMock}
-        key={keyMock}
-        onChange={handleSelectClick}
-        onRowClick={onRowClickMock}
-        refetchData={refetchDataMock}
-        rowActions={rowActionsMock}
-        select={selectMock}
-        selectedRows={selectedRowsMock}
-      />
-    )
     const row = screen.getByRole('row')
     expect(row).toHaveTextContent(refetchDataMock)
   })
 
+  it('should render table cell with correct data', () => {
+    const row = screen.getByRole('cell')
+    expect(row).toHaveTextContent(refetchDataMock)
+  })
+
   it('it should call handleSelectClick when checkbox is clicked', async () => {
-    render(
-      <EnhancedTableRow
-        columns={columsMock}
-        isSelection={isSelectionMock}
-        item={itemMock}
-        key={keyMock}
-        onChange={handleSelectClick}
-        onRowClick={onRowClickMock}
-        refetchData={refetchDataMock}
-        rowActions={rowActionsMock}
-        select={selectMock}
-        selectedRows={selectedRowsMock}
-      />
-    )
-    const checkbox = screen.getByTestId('menu-icon')
+    render(<Checkbox onChange={handleSelectClick} />)
+    const checkbox = screen.getByRole('checkbox')
     await userEvent.click(checkbox)
     expect(handleSelectClick).toHaveBeenCalled()
+  })
+
+  it('it should render action menu with item 1 when menu icon is clicked', async () => {
+    render(<MenuItem> {item1Mock} </MenuItem>)
+    const menuIcon = screen.getByTestId('menu-icon')
+    await userEvent.click(menuIcon)
+    expect(screen.getByText(item1Mock)).toBeInTheDocument()
+  })
+
+  it('it should render action menu with item 2 when menu icon is clicked', async () => {
+    render(<MenuItem> {item2Mock} </MenuItem>)
+    const menuIcon = screen.getByTestId('menu-icon')
+    await userEvent.click(menuIcon)
+    expect(screen.getByText(item2Mock)).toBeInTheDocument()
+  })
+
+  it('it should call onAction function when clicking on the menu item', async () => {
+    render(<MenuItem onClick={onActionMock}> {item1Mock} </MenuItem>)
+    const menuItem = screen.getByText(item1Mock)
+    await userEvent.click(menuItem)
+    expect(onActionMock).toHaveBeenCalledOnce()
+  })
+
+  it('it should close menu when "escape" is pressed', async () => {
+    render(
+      <Menu>
+        <MenuItem> {item1Mock} </MenuItem>
+        <MenuItem> {item2Mock} </MenuItem>
+      </Menu>
+    )
+    await userEvent.keyboard('{Escape}')
+    expect(screen.queryByText(item1Mock)).not.toBeInTheDocument()
+    expect(screen.queryByText(item2Mock)).not.toBeInTheDocument()
   })
 })
