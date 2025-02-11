9801a68070ee4f3697deca75b3ef1120fa05bf80
Anna Kravchuk <132332524+ANNA124kr@users.noreply.github.com>
2025-02-11 15:38:25 +0200

    create test question editor component (#192)

---
+++ src/tests/unit/components/question-editor/QuestionEditor.spec.jsx
@@ -0,0 +1,109 @@
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import QuestionEditor from '~/components/question-editor/QuestionEditor'
+
+const mockedData = {
+  type: 'multipleChoice',
+  text: '',
+  answers: [],
+  openAnswer: ''
+}
+
+const handleInputChangeMock = vi.fn()
+const handleNonInputValueChangeMock = vi.fn()
+const onCancelMock = vi.fn()
+const onEditMock = vi.fn()
+const onSaveMock = vi.fn()
+
+const renderComponent = (props = {}) =>
+  render(
+    <QuestionEditor
+      data={mockedData}
+      handleInputChange={handleInputChangeMock}
+      handleNonInputValueChange={handleNonInputValueChangeMock}
+      onCancel={onCancelMock}
+      onEdit={onEditMock}
+      onSave={onSaveMock}
+      {...props}
+    />
+  )
+
+describe('QuestionEditor', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+    renderComponent()
+  })
+
+  it('should renders question input field', () => {
+    const questionInput = screen.getByRole('textbox', { name: /question/i })
+    expect(questionInput).toBeInTheDocument()
+  })
+
+  it('it should renders a open answer', () => {
+    render(
+      <QuestionEditor
+        data={{ ...mockedData, type: 'openAnswer' }}
+        handleInputChange={handleInputChangeMock}
+        handleNonInputValueChange={handleNonInputValueChangeMock}
+        onCancel={onCancelMock}
+        onEdit={onEditMock}
+        onSave={onSaveMock}
+      />
+    )
+    const openAnswerInput = screen.getByRole('textbox', { name: /answer/i })
+    expect(openAnswerInput).toBeInTheDocument()
+  })
+
+  it('it should renders questionType multipleChoice', () => {
+    const select = screen.getByRole('combobox', {
+      value: 'questionPage.questionType.multipleChoice'
+    })
+    expect(select).toBeInTheDocument()
+  })
+
+  it('it should renders questionType openAnswer', () => {
+    const select = screen.getByRole('combobox', {
+      value: 'questionPage.questionType.openAnswer'
+    })
+    expect(select).toBeInTheDocument()
+  })
+
+  it('it should renders questionType oneAnswer', () => {
+    const select = screen.getByRole('combobox', {
+      value: 'questionPage.questionType.oneAnswer'
+    })
+    expect(select).toBeInTheDocument()
+  })
+
+  it('it should renders addNewOne ', () => {
+    const checkbox = screen.getByRole('checkbox', {
+      name: 'questionPage.addNewOne'
+    })
+    expect(checkbox).toBeInTheDocument()
+  })
+
+  it('should change question type', async () => {
+    const questionInput = screen.getByRole('textbox', { name: /question/i })
+    await userEvent.type(questionInput, 'text')
+    expect(handleInputChangeMock).toHaveBeenCalledWith('text')
+  })
+
+  it('should change answer input field', async () => {
+    render(
+      <QuestionEditor
+        data={{
+          ...mockedData,
+          type: 'openAnswer'
+        }}
+        handleInputChange={handleInputChangeMock}
+        handleNonInputValueChange={handleNonInputValueChangeMock}
+        onCancel={onCancelMock}
+        onSave={onSaveMock}
+      />
+    )
+    const openAnswerInput = screen.getByLabelText('questionPage.answer')
+    await userEvent.type(openAnswerInput, 'New answer')
+    expect(handleInputChangeMock).toHaveBeenCalledWith('openAnswer')
+  })
+})
