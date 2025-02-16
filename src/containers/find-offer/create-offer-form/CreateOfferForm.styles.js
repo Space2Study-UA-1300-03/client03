a395e278b48182597558540ad188b1ca43165634
NataliaKilienko <kilienko900@gmail.com>
2025-02-16 23:16:18 +0200

    Create modal layouts for offers and requests without logic

---
+++ src/containers/find-offer/create-offer-form/CreateOfferForm.styles.js
@@ -0,0 +1,68 @@
+export const styles = {
+  icon: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '30px',
+    height: '30px',
+    backgroundColor: '#f5f5f5',
+    borderRadius: '50%',
+    marginRight: '10px'
+  },
+  stepWrapper: {
+    pl: '34px',
+    pb: '24px'
+  },
+  title: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '24px',
+    color: '#455A64'
+  },
+  stepTitle: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '12px',
+    color: '#455A64'
+  },
+  subtitle: {
+    marginBottom: '24px',
+    color: '#455A64'
+  },
+  stepSubtitle: {
+    marginBottom: '6px',
+    color: '#607D8B'
+  },
+  img: {
+    width: '24px',
+    height: '24px',
+    marginRight: '10px'
+  },
+  select: {
+    mb: '14px'
+  },
+  summaryLength: {
+    mb: '6px',
+    textAlign: 'right',
+    color: '#607D8B',
+    fontSize: '14px'
+  },
+  chipsList: {
+    mt: '-10px',
+    mb: '16px'
+  },
+  sliderStyles: {
+    width: '260px'
+  },
+  btn: {
+    padding: '15px 20px',
+    display: 'flex',
+    width: 'calc(50% - 6px)',
+    columnGap: 1
+  },
+  btnsWrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    mb: '5px'
+  }
+}
